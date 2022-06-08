c3115aaaa7196f8865e59e0d7a257f7a7ea95b7c
Aleksey Vyuzhanin <doberxxx@gmail.com>
2022-06-08 11:07:42 +0300

    homework 4_3

--- 4_2.py
+++ 4_2.py
@@ -2,17 +2,23 @@
 
 import socket
 import time
+import json
+import yaml
 
-server_ip = {'drive.google.com':'', 'mail.google.com':'', 'google.com':''}
+srv_ip = {'drive.google.com':'', 'mail.google.com':'', 'google.com':''}
 print("Start cheking IP.")
 while(1==1):
-    for srv in server_ip.keys():
+    for srv in srv_ip.keys():
         ip = socket.gethostbyname(srv)
-        if(ip == server_ip[srv]):
+        if(ip == srv_ip[srv]):
             print(f'{srv} - {ip}')
-        elif(server_ip[srv]==''):
-            server_ip[srv] = ip
+        elif(srv_ip[srv]==''):
+            srv_ip[srv] = ip
         else:
-            print(f'[ERROR] {srv} IP mismatch: {server_ip[srv]} {ip}')
-            server_ip[srv] = ip
+            print(f'[ERROR] {srv} IP mismatch: {srv_ip[srv]} {ip}')
+            srv_ip[srv] = ip
+    with open ("dz43.json", 'w') as js_file:
+        js_file.write(json.dumps(srv_ip))
+    with open ("dz43.yml", 'w') as yaml_file:
+        yaml_file.write(yaml.dump(srv_ip))
     time.sleep(10)
